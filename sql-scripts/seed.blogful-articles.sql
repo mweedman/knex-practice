82a99e05a85eb2f2fc85a76a154cfe4ba9a5693d
Michael Weedman <michael.weedman@gmail.com>
2019-12-11 08:20:21 -0700

    added blogful initialization

---
+++ sql-scripts/seed.blogful-articles.sql
@@ -0,0 +1,9 @@
+INSERT INTO blogful_articles
+(title, content)
+VALUES
+('Blog 1', 'This is a great blog'),
+('Blog 1', 'This is a great blog'),
+('Blog 1', 'This is a great blog'),
+('Blog 1', 'This is a great blog'),
+('Blog 1', 'This is a great blog');
+
--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,69 @@
-console.log('hello, practice');
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+});
+
+console.log('knex and driver installed correctly');
+
+const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({name: 'Point of view gun'})
+  .first()
+  .toQuery();
+
+console.log(qry);
+
+
+
+function searchByProduceName (searchTerm) {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    })
+    .finally(() => knexInstance.destroy());
+}
+
+searchByProduceName('holo');
+
+function getProductsWithImages(){
+  knexInstance
+    .select('product_id', 'name', 'price', 'category', 'image')
+    .from('amazong_products')
+    .whereNotNull('image')
+    .then(res => {
+      console.log(res);
+    })
+    .finally(() => knexInstance.destroy());
+}
+
+getProductsWithImages();
+
+function mostPopularVideosForDays(days) {
+  knexInstance
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where(
+      'date_viewed',
+      '>',
+      knexInstance.raw('now() - \'?? days\'::INTERVAL', days)
+    )
+    .from('whopipe_video_views')
+    .groupBy('video_name', 'region')
+    .orderBy([
+      { column: 'region', order: 'ASC' },
+      { column: 'views', order: 'DESC' },
+    ])
+    .then(result => {
+      console.log(result);
+    })
+    .finally(() => knexInstance.destroy());
+}
+
+mostPopularVideosForDays(30);
